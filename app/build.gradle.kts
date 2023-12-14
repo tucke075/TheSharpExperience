e9c16b5e7fb94f2c14926c885120beefa5d88188
tucke075 <tucke075@csusm.edu>
2023-12-14 14:30:57 -0800

    Created a branch to implement nav bar with wanted fragments such as DayRN, NightRn, DayNA, NightNA.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.thesharpexperience"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.thesharpexperience"
