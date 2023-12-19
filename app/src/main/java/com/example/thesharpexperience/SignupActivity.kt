6aebaca27e3f2a0e5358798743ab79c00b95b5a5
tucke075 <tucke075@csusm.edu>
2023-12-18 23:12:29 -0800

    Added check boxes to signup page xml and changed signup logic for account creation in signup activity kt. Future goal: Implement the created recycler view adapater and its item design to display lists associated.

--- app/src/main/java/com/example/thesharpexperience/SignupActivity.kt
+++ app/src/main/java/com/example/thesharpexperience/SignupActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import com.example.thesharpexperience.R
@@ -15,9 +16,65 @@ import org.w3c.dom.Text
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+
+    //variables to hold check box values
+    private lateinit var dayCheck : CheckBox
+    private lateinit var nightCheck : CheckBox
+    private lateinit var RNCheck : CheckBox
+    private lateinit var NACheck : CheckBox
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        //variables of type checkBox to store live checked values
+        dayCheck  = findViewById(R.id.CBDays)
+        nightCheck  = findViewById(R.id.CBNights)
+        RNCheck  = findViewById(R.id.CB_RN)
+        NACheck = findViewById(R.id.CB_NA)
+
+        //bools to be sent to checkVals for checking purposes
+        var pair1: Boolean = false
+        var pair2: Boolean = false
+
+        //Redeclaring pair1 and pair 2 as false in box checks below incase
+            //signup button is pressed early and need to reset
+
+
+        //Set of two pairs of logic
+        //first pair actively listens to Days and Night check box to
+        //  make sure only one value is checked at once
+        dayCheck.setOnCheckedChangeListener { _, isChecked ->
+            pair1 = false
+            if(isChecked){
+                nightCheck.isChecked = false
+                pair1 = true
+            }
+        }
+        nightCheck.setOnCheckedChangeListener { _, isChecked ->
+            pair1 = false
+            if(isChecked){
+                dayCheck.isChecked = false
+                pair1 = true
+            }
+        }
+
+        //second pair actively listens to Registered Nurse and Nursing Assistant
+        // check box to make sure only one value is checked at once
+
+        RNCheck.setOnCheckedChangeListener { _, isChecked ->
+            pair2 = false
+            if(isChecked){
+                NACheck.isChecked = false
+                pair2 = true
+            }
+        }
+        NACheck.setOnCheckedChangeListener { _, isChecked ->
+            pair2 = false
+            if(isChecked){
+                RNCheck.isChecked = false
+                pair2 = true
+            }
+        }
 
         auth = FirebaseAuth.getInstance()
         findViewById<Button>(R.id.signup).setOnClickListener{
@@ -27,29 +84,44 @@ class SignupActivity : AppCompatActivity() {
             val name = findViewById<TextView>(R.id.name).text.toString().trim()
             //FIREBASE AUTH REQUIRES NON EMPTY EMAIL/PASSWORD FIELDS ALONG WITH AT LEAST A 6 CHARACTER PASSWORD
             val passwordSize = password.length
-            if(password != confirmpassword)
+
+            val nullcheck = checkVals(pair1, pair2, password, confirmpassword, email, name)
+
+            if(nullcheck == false)
+                Toast.makeText(this, "Cannot have empty fields", Toast.LENGTH_SHORT).show()
+            else if(password != confirmpassword)
                 Toast.makeText(this, "Password do not match", Toast.LENGTH_SHORT).show()
             else if (passwordSize < 8)
                 Toast.makeText(this, "Password must be 8 characters long", Toast.LENGTH_SHORT).show()
-            else if (password.isEmpty() || confirmpassword.isEmpty() || email.isEmpty())
-                Toast.makeText(this, "Cannot have empty fields", Toast.LENGTH_SHORT).show()
-            else if(password == confirmpassword){ //passes requirements and password match
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {  //email doesnt exist and firebase authentication accepts it
+            else
+                {
+                    if(password == confirmpassword){ //passes requirements and password match
+                        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                            if (task.isSuccessful) {  //email doesnt exist and firebase authentication accepts it
 
-                        //store sighnup values into firebase firestore and make an account associated
-                        addToFirebase(email, name)
+                            //store sighnup values into firebase firestore and make an account associated
+                            addToFirebase(email, name)
 
-                        startActivity(Intent(applicationContext, FragDisplayActivity::class.java))
-                    } else {
-                        Toast.makeText(this, "Not created", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(applicationContext, FragDisplayActivity::class.java))
+                            } else {
+                                Toast.makeText(this, "Not created", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                     }
                 }
-            }
 
         }
     }
-    private fun addToFirebase(email : String,name : String){
+
+
+    private fun checkVals(pair1:Boolean, pair2: Boolean, password : String, confirmPassword : String, email: String, name: String) : Boolean
+    {
+        if(pair1 == false || (pair2 == false) || password.isEmpty() || confirmPassword.isEmpty() || email.isEmpty() || name.isEmpty())
+            return false
+        else
+            return true
+    }
+    private fun addToFirebase(email : String,name : String) {
         val db = FirebaseFirestore.getInstance()
         val user: MutableMap<String, Any> = HashMap()
         user["email"] = email
