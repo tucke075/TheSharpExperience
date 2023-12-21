fdc773e421106aae7c3876ec53fb12f6162c75ad
tucke075 <tucke075@csusm.edu>
2023-12-21 13:59:05 -0800

    PoC working drawer navigation along with adding a security feature where only a lead can modify someones hire date using the function setUpDrawer().

--- app/src/main/java/com/example/thesharpexperience/FragDisplayActivity.kt
+++ app/src/main/java/com/example/thesharpexperience/FragDisplayActivity.kt
@@ -3,21 +3,25 @@ package com.example.thesharpexperience
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar //needed for toolbar, HAS TO BE ANDROID X
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.example.thesharpexperience.Drawer_Activities.SettingsActivity
 import com.example.thesharpexperience.bot_nav_fragments.dayNaFragment
 import com.example.thesharpexperience.bot_nav_fragments.dayRnFragment
 import com.example.thesharpexperience.bot_nav_fragments.nightNaFragment
 import com.example.thesharpexperience.bot_nav_fragments.nightRnFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FragDisplayActivity : AppCompatActivity() {
-    //private lateinit var drawerLayout : DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,66 +32,135 @@ class FragDisplayActivity : AppCompatActivity() {
         val nightRN = nightRnFragment()
         val nightNA = nightNaFragment()
 
-        val toolbar : Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
-
-        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        setUpDrawer()
+        //set onclick listener for items in the drawer navigation view (nav_view in frag_activity xml
+        /*navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.hireDate -> {
+                    startActivity(Intent(applicationContext, SettingsActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }*/
+
+
         changeFragment(dayRN)
-        findViewById<BottomNavigationView>(R.id.bot_nav).setOnItemSelectedListener{ item ->
+        findViewById<BottomNavigationView>(R.id.bot_nav).setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.ic_day_rn -> {
                     changeFragment(dayRN)
                     true
                 }
+
                 R.id.ic_day_na -> {
                     changeFragment(dayNA)
                     true
                 }
+
                 R.id.ic_night_rn -> {
                     changeFragment(nightRN)
                     true
                 }
+
                 R.id.ic_night_na -> {
                     changeFragment(nightNA)
                     true
                 }
+
                 else -> false
             }
         }
-        findViewById<FloatingActionButton>(R.id.addButton).setOnClickListener{
+
+        findViewById<FloatingActionButton>(R.id.addButton).setOnClickListener {
             startActivity(Intent(this, RequestAddActivity::class.java))
         }
-        /*findViewById<FloatingActionButton>(R.id.bSignout).setOnClickListener{
-            logout()
-        }
-
-         */
 
     }
 
     //always needed for navigation drawer
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item))
+        if(toggle.onOptionsItemSelected(item)) {
             return true
+        }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun changeFragment(fragment: Fragment){
+
+    private fun setUpDrawer() {
+        val navigationView: NavigationView = findViewById(R.id.nav_view) // Make sure this is the correct ID for your NavigationView
+        val hireDateMenuItem = navigationView.menu.findItem(R.id.hireDate)
+        checkLead{isLead ->
+            if(!isLead)
+                hireDateMenuItem.isVisible=false
+
+
+        }
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.hireDate -> {
+                    checkLead { isLead ->
+                        if (isLead) {
+                            // Perform action for hire date item
+                        } else {
+                            Toast.makeText(this, "Access Denied", Toast.LENGTH_SHORT).show()
+                            menuItem.isVisible = false
+                        }
+                    }
+                    true // Indicate that the click event has been handled
+                }
+                // Handle other menu items here
+                // ...
+            }
+            true // Close drawer when an item is clicked
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fragmentContainerView, fragment)
             commit()
         }
     }
-    private fun logout()
-    {
+
+    private fun logout() {
         FirebaseAuth.getInstance().signOut()
         startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun checkLead(callback: (Boolean) -> Unit) {
+        var lead: Int = -1
+        //checking firebase authentication associated with login
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) //user exists
+        {
+            //open cloud firestore and check if person is lead
+            val db = FirebaseFirestore.getInstance()
+            db.collection("Days")
+                .get()
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        for (document in it.result!!) {
+                            if (user.email == document.data["email"].toString()) {
+                                lead = document.data["nurseType"]?.toString()?.toIntOrNull() ?: -1
+                            }
+                        }
+                        callback(lead == 2)
+                    } else {
+                        callback(false)
+                    }
+                }
+        } else {
+            callback(false)
+        }
     }
 }
